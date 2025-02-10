049a9d36677edc77c2af9d35bc8d4ec70e967edf
Spencer_Durfey <durfey_32@yahoo.com>
2025-02-10 11:39:08 -0700

    updated resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,33 +7,76 @@ export default function Resume() {
                 <a className="download" href="/SpencerDurfeyResume.pdf" download="SpencerDurfeyResume.pdf">Click Here to Download My Resume</a>
             </div>
             <div className="skillsList">
-                <h2>Front End Proficiencies</h2>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>jQuery</li>
-                    <li>responsive design</li>
-                    <li>React</li>
-                    <li>Bootstrap</li>
-                </ul>
+        <h2>Front End Proficiencies</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Bootstrap</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>React.js</li>
+            <li>jQuery</li>
+            <li>AJAX</li>
+        </ul>
 
-                <h2>Back End Proficiencies</h2>
-                <ul>
-                    <li>APIs</li>
-                    <li>Node</li>
-                    <li>Express</li>
-                    <li>MySQL, Sequelize</li>
-                    <li>REST</li>
-                    <li>GraphQL</li>
-                </ul>
-                <h2>Dev Tools</h2>
-                <ul>
-                    <li>Git</li>
-                    <li>npm</li>
-                </ul>
+        <h2>Back End Proficiencies</h2>
+        <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>REST APIs</li>
+            <li>Sequelize</li>
+            <li>bcrypt</li>
+            <li>JSON Web Token (JWT)</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>PostgreSQL</li>
+            <li>NoSQL</li>
+            <li>PyMongo</li>
+            <li>Python</li>
+            <li>YAML</li>
+            <li>APIs</li>
+        </ul>
 
-            </div>
+        <h2>Tools and Methodologies</h2>
+        <ul>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>GitHub Actions</li>
+            <li>CI/CD</li>
+            <li>CircleCI</li>
+            <li>Command Line Interface</li>
+            <li>Insomnia</li>
+            <li>Agile Methodologies</li>
+            <li>Cypress</li>
+            <li>DOM</li>
+            <li>Developer Tools</li>
+            <li>MongoDB Compass</li>
+            <li>Prompt Engineering</li>
+            <li>Responsive Web Design</li>
+            <li>Test-Driven Development (TDD)</li>
+            <li>Vite</li>
+            <li>Visual Studio Code</li>
+            <li>Web Accessibility</li>
+            <li>Wireframing</li>
+        </ul>
+
+        <h2>Professional Skills</h2>
+        <ul>
+            <li>Problem-Solving</li>
+            <li>Research Skills</li>
+            <li>Time Management</li>
+            <li>Collaboration</li>
+            <li>Documentation Review</li>
+            <li>Operational Efficiency</li>
+        </ul>
+
+        <h2>Specialized Systems</h2>
+        <ul>
+            <li>Laboratory Information Management Systems (LIMS)</li>
+            <li>HPLC-MS</li>
+            <li>Mass Spectrometer</li>
+        </ul>
+    </div>
         </>
     );
 }
